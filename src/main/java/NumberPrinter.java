dba4195706d6dd37b1b7d872e94359bd356376ba
skarning <sivertskarning@hotmail.com>
2019-10-11 21:08:26 +0200

    Completed the algorithm for printing Hi, HiOf and Of when number is divideable with 5,3 and 5 and 3

--- src/main/java/NumberPrinter.java
+++ src/main/java/NumberPrinter.java
@@ -8,8 +8,18 @@ public class NumberPrinter {
     }
 
     public String getNumber(int inputNumber) {
+        if (((inputNumber % 3) == 0) && (inputNumber % 5) == 0)
+            return "HiOf";
         if ((inputNumber % 3) == 0)
-            return "HI";
+            return "Hi";
+        if ((inputNumber % 5) == 0)
+            return "Of";
         return "" + inputNumber;
+    }
+
+    public void printNumbers() {
+        for(int i = from; i <= to; i++) {
+            System.out.println(getNumber(i));
+        }
     }
 }
