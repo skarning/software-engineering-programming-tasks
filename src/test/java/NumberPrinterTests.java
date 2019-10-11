5ec6215322fd617e759bdcceee7fa7f188dd7edd
skarning <sivertskarning@hotmail.com>
2019-10-11 20:35:16 +0200

    Changed test to test the three first numbers of test case

--- src/test/java/NumberPrinterTests.java
+++ src/test/java/NumberPrinterTests.java
@@ -4,8 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class NumberPrinterTests {
 
     @Test
-    public void getListOfNumbersTest() {
+    public void getfNumbersTest() {
         NumberPrinter numberPrinter = new NumberPrinter(0, 100);
-        assertEquals(0, numberPrinter.getListOfNumbers());
+        assertEquals(0, numberPrinter.getNumber(0));
+        assertEquals(1, numberPrinter.getNumber(1));
+        assertEquals(2, numberPrinter.getNumber(2));
     }
 }
