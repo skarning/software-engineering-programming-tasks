4ca8f7302eb731bedd44f1a3702d26a33a54ce3b
skarning <sivertskarning@hotmail.com>
2019-10-11 20:37:33 +0200

    Changed function getListOfNumbers to getNumbers() wich return the three first numbers of the 0-100 list

--- src/main/java/NumberPrinter.java
+++ src/main/java/NumberPrinter.java
@@ -7,7 +7,13 @@ public class NumberPrinter {
         this.to = to;
     }
 
-    public int getListOfNumbers() {
+    public int getNumber(int inputNumber) {
+        if (inputNumber == 0)
+            return 0;
+        if (inputNumber == 1)
+            return 1;
+        if (inputNumber == 2)
+            return 2;
         return 0;
     }
 }
