2acf5cc34e46d8c17cce7cec89806d8443347c5b
skarning <sivertskarning@hotmail.com>
2019-10-11 20:15:49 +0200

    Added objecte instantiation of NumberPrinter in main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String []args) {
-        System.out.println("Hello World!!");
+        NumberPrinter numberPrinter = new NumberPrinter(0, 100);
     }
 }
