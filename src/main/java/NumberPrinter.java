4b805a60437398da057787761762a8dfd176a131
skarning <sivertskarning@hotmail.com>
2019-10-11 20:49:46 +0200

    Added functionality for printing HI when input number is dividable by 3

--- src/main/java/NumberPrinter.java
+++ src/main/java/NumberPrinter.java
@@ -7,13 +7,9 @@ public class NumberPrinter {
         this.to = to;
     }
 
-    public int getNumber(int inputNumber) {
-        if (inputNumber == 0)
-            return 0;
-        if (inputNumber == 1)
-            return 1;
-        if (inputNumber == 2)
-            return 2;
-        return 0;
+    public String getNumber(int inputNumber) {
+        if ((inputNumber % 3) == 0)
+            return "HI";
+        return "" + inputNumber;
     }
 }
