054f407e296995366eae6303e8bfe48784b14d82
skarning <sivertskarning@hotmail.com>
2019-10-11 20:50:49 +0200

    Changed test to test if numbers dividable by 3 returns HI

--- src/test/java/NumberPrinterTests.java
+++ src/test/java/NumberPrinterTests.java
@@ -4,10 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class NumberPrinterTests {
 
     @Test
-    public void getfNumbersTest() {
+    public void getNumbersTest() {
         NumberPrinter numberPrinter = new NumberPrinter(0, 100);
-        assertEquals(0, numberPrinter.getNumber(0));
-        assertEquals(1, numberPrinter.getNumber(1));
-        assertEquals(2, numberPrinter.getNumber(2));
+        assertEquals("HI", numberPrinter.getNumber(0));
+        assertEquals("1", numberPrinter.getNumber(1));
+        assertEquals("2", numberPrinter.getNumber(2));
+        assertEquals("HI", numberPrinter.getNumber(3));
     }
 }
