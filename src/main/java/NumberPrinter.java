5b65e868b5d75881f96dab09275e6d2bf15b6712
skarning <sivertskarning@hotmail.com>
2019-10-11 20:16:40 +0200

    Added first refactor of getListOfNumbers

---
+++ src/main/java/NumberPrinter.java
@@ -0,0 +1,13 @@
+public class NumberPrinter {
+    private int from;
+    private int to;
+
+    public NumberPrinter(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public int getListOfNumbers() {
+        return 0;
+    }
+}
